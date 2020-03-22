690621c620a698f3e4441cfb1dabf0291053ca2e
nfwyst <nfwyst@gmail.com>
2020-03-22 16:53:04 +0800

    marvin@update

--- 缓存服务器/对比缓存/etag.js
+++ 缓存服务器/对比缓存/etag.js
@@ -14,19 +14,25 @@ http
         return res.end(err.message)
       }
       const ifNoneMatch = req.headers['if-none-match']
-      fs.readFile(filepath, (err, content) => {
-        if (err) {
+      const rs = fs.createReadStream(filepath)
+      const md5Hash = crypto.createHash('md5')
+      rs
+        .on('data', data => {
+          md5Hash.update(data)
+        })
+        .on('end', () => {
+          const etag = md5Hash.digest('hex')
+          if (etag === ifNoneMatch) {
+            res.writeHead(304)
+            return res.end('')
+          }
+          res.setHeader('ETag', etag)
+          res.setHeader('Content-Type', mime.getType(filepath))
+          fs.createReadStream(filepath).pipe(res)
+        })
+        .on('error', (err) => {
           res.statusCode = 500
-          return res.end(err.message)
-        }
-        const etag = crypto.createHash('md5').update(content).digest('hex')
-        if (etag === ifNoneMatch) {
-          res.writeHead(304)
-          return res.end('')
-        }
-        res.setHeader('ETag', etag)
-        res.setHeader('Content-Type', mime.getType(filepath))
-        fs.createReadStream(filepath).pipe(res)
-      })
+          res.end(err.message)
+        })
     })
   })
