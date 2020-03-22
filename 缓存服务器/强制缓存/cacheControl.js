7a5d99595a38c3d3ef28b79c2b11f31bfc973a3d
nfwyst <nfwyst@gmail.com>
2020-03-22 17:29:25 +0800

    marvin@update

---
+++ 缓存服务器/强制缓存/cacheControl.js
@@ -0,0 +1,22 @@
+const http = require('http')
+const url = require('url')
+const path = require('path')
+const fs = require('fs')
+const mime = require('mime')
+
+http
+  .createServer(function (req, res) {
+    const { pathname } = url.parse(req.url)
+    if (pathname === '/favicon.ico') return res.end()
+    const filepath = path.resolve(__dirname, `.${pathname}`)
+    fs.stat(filepath, err => {
+      if (err) {
+        res.statusCode = 500
+        return res.end(err.message)
+      }
+      res.setHeader('Content-Type', mime.getType(filepath))
+      res.setHeader('Cache-Control', 'max-age=10')
+      fs.createReadStream(filepath).pipe(res)
+    })
+  })
+  .listen(8080, () => console.log('server started on port 8080'))
