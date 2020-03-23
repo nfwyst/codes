c27bc5dbaaacf164d0030ccead1b8f400b33b550
nfwyst <nfwyst@gmail.com>
2020-03-23 17:32:04 +0800

    marvin@update

--- 代理服务器/proxy.js
+++ 代理服务器/proxy.js
@@ -1,12 +1,29 @@
 const http = require('http')
-const proxy = require('http-proxy')
+const url = require('url')
 
-const server = http.createServer(function (req, res) {
-  proxy.createProxyServer().web(req, res, {
-    target: 'http://localhost:8000'
+class WebProxy {
+  constructor({ target }) {
+    this.target = target
+  }
+
+  web(req, res) {
+    const from = url.parse(req.url)
+    const to = url.parse(this.target)
+    const { method, headers } = req
+    const options = { ...from, ...to, method, headers }
+    const request = http.request(options)
+    request
+      .on('response', resp => resp.pipe(res))
+      .on('error', err => res.end(err.message))
+    req.pipe(request)
+    request.end()
+  }
+}
+
+const proxy = new WebProxy({ target: 'http://localhost:8000' })
+
+http
+  .createServer((req, res) => proxy.web(req, res))
+  .listen(9000, () => {
+    console.log('proxy server is running on port 9000')
   })
-})
-
-server.listen(9000, () => {
-  console.log('proxy server is running on port 9000')
-})
