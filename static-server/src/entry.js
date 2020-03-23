732a13a89fed6f22af3a367f56b0903f6876d929
nfwyst <nfwyst@gmail.com>
2020-03-23 12:02:23 +0800

    marvin@update

--- static-server/src/entry.js
+++ static-server/src/entry.js
@@ -9,6 +9,7 @@ const fs = require('fs')
 const mime = require('mime')
 const art = require('art-template')
 const zlib = require('zlib')
+const crypto = require('crypto')
 
 const stat = promisify(fs.stat)
 const readdir = promisify(fs.readdir)
@@ -53,7 +54,7 @@ class Server {
         res.setHeader('Content-Type', 'text/html')
         res.end(html)
       } else {
-        this.sendFile(req, res, filepath)
+        this.sendFile(req, res, filepath, stats)
       }
     } catch (e) {
       debug(inspect(e, false, 3, true))
@@ -66,11 +67,45 @@ class Server {
     res.end(`something wrong...`)
   }
 
-  sendFile(req, res, filepath) {
+  sendFile(req, res, filepath, stats) {
+    let isNoneMatch = req.headers['is-none-match']
+    let ifModifiedSince = req.headers['if-modified-since']
+    res.setHeader('Cache-Control', 'private, max-age=30')
+    res.setHeader('Expires', new Date(Date.now() + 30 * 1000))
+
+    // 缓存
+    if (isNoneMatch) {
+      const rs = fs.createReadStream(filepath)
+      const md5Hash = crypto.createHash('md5')
+      rs
+        .on('data', data => md5Hash.update(data))
+        .on('end', () => {
+          const etag = md5Hash.digest('hex')
+          if (etag === isNoneMatch) {
+            res.writeHead(304)
+            return res.end('')
+          }
+          res.setHeader('ETag', etag)
+          this._sendFile(req, res, filepath)
+        })
+    } else if (ifModifiedSince) {
+      if (ifModifiedSince === stats.ctime.toGMTString()) {
+        res.writeHead(304)
+        return res.end('')
+      }
+      res.setHeader('Last-Modified', stats.ctime.toGMTString())
+      this._sendFile(req, res, filepath)
+    } else {
+      this._sendFile(req, res, filepath)
+    }
+  }
+
+  _sendFile(req, res, filepath) {
     res.statusCode = 200
     res.setHeader('Content-Type', mime.getType(filepath))
     const acceptEncoding = req.headers['accept-encoding']
     let transfer = null
+    // 压缩
     if (/\bgzip\b/.test(acceptEncoding)) {
       transfer = zlib.createGzip()
     } else if (/\bdeflate\b/.test(acceptEncoding)) {
