f06b3665fef632721d26cb11b9c43e6ff0aacf8b
nfwyst <nfwyst@gmail.com>
2020-03-25 17:09:43 +0800

    marvin@update

--- 模板引擎/index.js
+++ 模板引擎/index.js
@@ -1,24 +1,30 @@
-const obj = { user: { name: 'bob', age: 99 } }
+const fs = require('fs')
+const path = require('path')
 
-
-function render(str, data) {
-  const head = 'let tpl = ``; \r\nwith(obj) {\r\n tpl += `'
-  str = str.replace(/<%([\s\S]+?)%>/g, function () {
-    return '`;' + arguments[1] + '\r\n' + 'tpl +=`'
+function render(filepath, data, callback) {
+  fs.readFile(filepath, 'utf8', (err, str) => {
+    if (err) return callback(err)
+    try {
+      const head = 'let tpl = ``; \r\nwith(obj) {\r\n tpl += `'
+      str = str.replace(/<%=([\s\S]+?)%>/g, function () {
+        return "${" + arguments[1] + "}"
+      })
+      str = str.replace(/<%([\s\S]+?)%>/g, function () {
+        return '`;' + arguments[1] + '\r\n' + 'tpl +=`'
+      })
+      const tail = '`\r\n} \r\nreturn tpl\r\n'
+      const html = head + str + tail
+      const fn = new Function('obj', html)
+      const result = fn(data)
+      callback(null, result)
+    } catch (e) {
+      callback(e)
+    }
   })
-  const tail = '`\r\n} \r\nreturn tpl\r\n'
-  const html = head + str + tail
-  console.log(html)
-  const fn = new Function('obj', html)
-  return fn(data)
 }
 
-const str = `
-  <% if(user) { %>
-    hello user
-  <% } else { %>
-    hello guest
-  <% } %>
-`
-const result = render(str, obj)
-console.log(result)
+const obj = { user: { name: 'bob', age: 99 }, total: 5 }
+render(path.resolve(__dirname, './index.html'), obj, (err, result) => {
+  if (err) return console.log(err)
+  console.log(result)
+})
