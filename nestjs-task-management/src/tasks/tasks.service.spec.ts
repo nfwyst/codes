6572dcb473e8624fa5a2a0e3840b1f48fc74f4bc
nfwyst <nfwyst@gmail.com>
2020-04-29 17:20:28 +0800

    nfwyst@update

--- nestjs-task-management/src/tasks/tasks.service.spec.ts
+++ nestjs-task-management/src/tasks/tasks.service.spec.ts
@@ -4,10 +4,14 @@ import { TasksService } from './tasks.service';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatus } from './types/tasks-status.enum';
 import { User } from '../auth/user.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { NotFoundException } from '@nestjs/common';
 
 const mockTaskRepository = () => ({
   getTasks: jest.fn(),
   findOne: jest.fn(),
+  createTask: jest.fn(),
+  delete: jest.fn(),
 })
 
 describe('TaskService', () => {
@@ -63,6 +67,39 @@ describe('TaskService', () => {
         username: 'Test user',
         id: 12
       } as User)).rejects.toThrow()
+    })
+  })
+
+  describe('createTask', () => {
+    it('calls taskRepository.createTask() and return the result', async () => {
+      taskRepository.createTask.mockResolvedValue('someTask')
+      expect(taskRepository.createTask).not.toHaveBeenCalled()
+      const createTaskDto: CreateTaskDto = {
+        title: 'hello', description: 'world'
+      }
+      const mockUser = {
+        username: 'Test user',
+      }
+      const result = await taskService.createTask(createTaskDto, mockUser as User)
+      expect(taskRepository.createTask).toHaveBeenCalledWith(createTaskDto, mockUser)
+      expect(result).toEqual('someTask')
+    })
+  })
+
+  describe('deleteTaskById', () => {
+    it('calls taskRepository.delete() to delete a task', async () => {
+      taskRepository.delete.mockResolvedValue({ affected: 1 })
+      expect(taskRepository.delete).not.toHaveBeenCalled()
+      await taskService.deleteTaskById(1, { id: 2 } as User)
+      expect(taskRepository.delete).toHaveBeenCalledWith({
+        id: 1, userId: 2
+      })
+    })
+
+    it('throws an error as task is not found', async () => {
+      taskRepository.delete.mockResolvedValue({ affected: 0 })
+      expect(taskRepository.delete).not.toHaveBeenCalled()
+      expect(taskService.deleteTaskById(1, { id: 2 } as User)).rejects.toThrow(NotFoundException)
     })
   })
 })
