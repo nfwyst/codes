ac05dd64706adfb360297a934fdfd8798616a81c
nfwyst <nfwyst@gmail.com>
2020-05-11 13:42:19 +0800

    update

---
+++ 算法/insert-sort-stack.js
@@ -0,0 +1,14 @@
+//stack<int> R:
+// stack<int> S:
+// int t = R.pop();
+// while(!R.empty()) {
+//   if(S.empty() || S.top() <= t) {
+//     S.push(t)
+//     t = R.pop()
+//   } else {
+//     R.push(S.pop())
+//   }
+// }
+// S.push(t)
+// return S
+//}
